ee06ab8cb56d50c2547845d38cf33689c874244a
zhaijiayu572 <zjypersonal@163.com>
2016-12-10 22:15:21 +0800

    完成了一些php的算法练习

--- upload.php
+++ upload.php
@@ -1,8 +1,25 @@
 <?php
     if(isset($_POST['sub'])){
         $file = $_FILES['file'];
-        var_dump($file);
-        $name = $file[''];
+        $name = $file['name'];
+        $arr = ['jpg','docx','txt'];
+        $isIllegality = false;
+        $a = explode('.',$name);
+        $number = count($a)-1;
+        $str = $a[$number];
+
+        for($i=0;$i<count($arr);$i++){
+            if($str==$arr[$i]){
+                $isIllegality = true;
+                break;
+            }
+        }
+        if(!$isIllegality){
+            move_uploaded_file($file['tmp_name'],"./img/$name");
+        }else{
+            echo "上传文件非法";
+        }
+
     }
 
 ?>
