63fa4c29ed5468dc7dea2c43dbcaea052a8615c9
zhaijiayu572 <zjypersonal@163.com>
2016-12-23 19:44:25 +0800

    增加了登录返回原页面功能，用户在原页面跳转登录，登陆成功时会跳转回原先的页面

--- blog/login.php
+++ blog/login.php
@@ -1,41 +1,48 @@
 <?php
 include "conect.php";
+
 if(isset($_POST['sub'])){
     $name = $_POST['name'];
     $pass = $_POST['pass'];
-    $arr = ['=','%','&'];
-    $bFlag = true;
+    $arr = ['=','&','%'];
+    $isLegal = true;
+    $url = $_POST['url'];
+    $address = "";
+    if(!empty($url)){
+        $address = $url;
+    }else{
+        $address = 'index.php';
+    }
     for($i=0;$i<strlen($pass);$i++){
         for($j=0;$j<count($arr);$j++){
             if($pass[$i]==$arr[$j]){
-                $bFlag = false;
+                $isLegal = false;
             }
         }
     }
-    if($bFlag){
+    if($isLegal){
         $sql = "select * from user where name='$name' and pass='$pass'";
         $query = mysqli_query($link,$sql);
         $result = mysqli_fetch_array($query);
         if($result){
-            setcookie('id',$result['id']);
+            setcookie('id',$result['id'],time()+60);
             setcookie('name',$result['name']);
-            echo "<script>alert('欢迎你".$result['name']."')</script>";
-            echo "<script>location='index.php'</script>";
+                echo "<script>alert('登录成功！')</script>";
+                echo "<script>location='".$address."'</script>";
         }else{
-            echo "<script>alert('用户名或密码错误')</script>";
+            echo "<script>alert('登录失败')</script>";
             echo "<script>location='login.php'</script>";
         }
     }else{
-        echo "<script>alert('输入的密码非法')</script>";
+        echo "<script>alert('密码非法')</script>";
         echo "<script>location='login.php'</script>";
     }
 }
 ?>
 <meta charset="utf-8">
 <form action="login.php" method="post">
+    <input type="hidden" value="<?php echo $_GET['url']?>" name="url">
     用户名:<input type="text" name="name"><br>
     密码:<input type="password" name="pass"><br>
-    <input type="submit" name="sub">
+    <input type="submit" value="登录" name="sub">
 </form>
-<a href="reg.php">注册</a>
-
