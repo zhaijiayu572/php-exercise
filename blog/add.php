1be1fda23127a3c2ff06a300df95e96e25132f7a
zhaijiayu572 <zjypersonal@163.com>
2016-12-23 19:38:28 +0800

    在发布文章时可以选择标签

--- blog/add.php
+++ blog/add.php
@@ -1,21 +1,45 @@
 <?php
 include "conect.php";
+if(isset($_COOKIE['id'])){
     if(isset($_POST['sub'])){
+        $id = $_COOKIE['id'];
+        $content = $_POST['content'];
         $title = $_POST['title'];
-        $con = $_POST['con'];
-        $sql = "insert into blog(bid,title,content,time) values(null,'$title','$con',now())";
+        $ca = $_POST['ca'];
+        $sql = "insert into blog(title,content,time,cid,id) values('$title','$content',now(),'$ca','$id')";
         $query = mysqli_query($link,$sql);
         if($query){
-            echo "<script>alert('更新数据成功')</script>";
+            echo "<script>alert('发表成功！')</script>";
             echo "<script>location='index.php'</script>";
         }else{
-            echo "error";
+            echo "<script>alert('发表失败')</script>";
+            echo "<script>location='index.php'</script>";
         }
     }
+}else{
+    $url = $_SERVER['REQUEST_URI'];
+    $url = explode('/',$url);
+    $last = count($url)-1;
+    $url = $url[$last];
+    echo "<script>alert('尚未登录,请登录')</script>";
+    echo "<script>location='login.php?url=$url'</script>";
+}
 ?>
 <meta charset="utf-8">
-<form action="add.php" method="post">
+<form method="post" action="add.php">
     标题:<input type="text" name="title">
-    内容:<textarea cols="20" rows="10" name="con"></textarea>
-    <input type="submit" name="sub" value="添加">
+    <select name="ca">
+        <?php
+            $sql = "select * from catalog ";
+            $query = mysqli_query($link,$sql);
+            while ($arr = mysqli_fetch_array($query)){
+                ?>
+                <option value="<?php echo $arr['cid']?>"><?php echo $arr['cname']?></option>
+                <?php
+            }
+        ?>
+    </select>
+    <br>
+    内容:<textarea name="content" cols="30" rows="10"></textarea>
+    <input type="submit" name="sub" value="发表">
 </form>
