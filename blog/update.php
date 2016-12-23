bf21e4268a598f66289625ab53e3c67f1b7fc203
zhaijiayu572 <zjypersonal@163.com>
2016-12-23 19:45:07 +0800

    增加了修改权限，只有原作者才能修改文章

--- blog/update.php
+++ blog/update.php
@@ -1,16 +1,34 @@
 <?php
 include "conect.php";
-if(isset($_GET['id'])){
-    $id = $_GET['id'];
-    $sql = "select * from blog where bid='$id'";
-    $query = mysqli_query($link,$sql);
-    $arr = mysqli_fetch_array($query);
+if(isset($_GET['bid'])){
+    $authorId = $_GET['id'];
+    $loginId = $_COOKIE['id'];
+    $bid = $_GET['bid'];
+    if(isset($_COOKIE['id'])){
+        if($authorId == $loginId){
+            $sql = "select * from blog where bid='$bid'";
+            $query = mysqli_query($link,$sql);
+            $arr = mysqli_fetch_array($query);
+        }else{
+            echo "<script>alert('您不是作者，无权更改')</script>";
+            echo "<script>location='index.php'</script>";
+        }
+    }else{
+        $url = $_SERVER['REQUEST_URI'];
+        $url = explode('/',$url);
+        $last = count($url)-1;
+        $url = $url[$last];
+        echo "<script>alert('尚未登录,请登录')</script>";
+        echo "<script>location='login.php?url=$url'</script>";
+    }
+
+
 }
 if(isset($_POST['sub'])){
     $title = $_POST['title'];
     $con = $_POST['con'];
-    $hid = $_POST['hid'];
-    $sql = "update blog set title='$title',content='$con' where bid='$hid'";
+    $bid = $_POST['bid'];
+    $sql = "update blog set title='$title',content='$con' where bid='$bid'";
     $query = mysqli_query($link,$sql);
     if($query){
         echo "<script>alert('更新数据成功')</script>";
@@ -20,7 +38,7 @@ if(isset($_POST['sub'])){
 ?>
 <meta charset="utf-8">
 <form action="update.php" method="post">
-    <input type="hidden" name="hid" value="<?php echo $id?>">
+    <input type="hidden" name="bid" value="<?php echo $bid?>">
     标题:<input type="text" name="title" value="<?php echo $arr['title']?>">
     内容:<textarea cols="20" rows="10" name="con"><?php echo $arr['content']?></textarea>
     <input type="submit" name="sub" value="添加">
